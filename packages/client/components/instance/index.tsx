b57bb7ec0a0171f1c9060eb75de229a8de80e1fa
Gtoasted <git@gtoasted.de>
2026-03-05 19:36:19 +0100

    refactor: use Instance.maxEmoji instead of CONFIGURATION.MAX_EMOJI
    Signed-off-by: Gtoasted <git@gtoasted.de>

--- packages/client/components/instance/index.tsx
+++ packages/client/components/instance/index.tsx
@@ -15,6 +15,7 @@ export function InstanceContext(props: { children: JSXElement }) {
           CONFIGURATION.DEFAULT_PROXY_URL,
           CONFIGURATION.DEFAULT_GIFBOX_URL,
           CONFIGURATION.HCAPTCHA_SITEKEY,
+          CONFIGURATION.MAX_EMOJI,
         )
       }
     >
