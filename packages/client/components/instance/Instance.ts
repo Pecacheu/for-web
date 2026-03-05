d05ab2d6020db254e10320f70ba2b08bed7e8ea0
Gtoasted <git@gtoasted.de>
2026-03-05 16:37:59 +0100

    refactor: use Instance.wsUrl instead of CONFIGURATION.DEFAULT_WS_URL
    Signed-off-by: Gtoasted <git@gtoasted.de>

--- packages/client/components/instance/Instance.ts
+++ packages/client/components/instance/Instance.ts
@@ -1,7 +1,9 @@
 export default class Instance {
   readonly apiUrl: string;
+  readonly wsUrl: string;
 
-  constructor(apiUrl: string) {
+  constructor(apiUrl: string, wsUrl: string) {
     this.apiUrl = apiUrl;
+    this.wsUrl = wsUrl;
   }
 }
