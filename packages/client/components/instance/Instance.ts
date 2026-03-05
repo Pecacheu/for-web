e6cb913df53c834dc97acf0a25354f1f1badae5a
Gtoasted <git@gtoasted.de>
2026-03-05 16:47:14 +0100

    refactor: use Instance.mediaUrl instead of CONFIGURATION.DEFAULT_MEDIA_URL
    Signed-off-by: Gtoasted <git@gtoasted.de>

--- packages/client/components/instance/Instance.ts
+++ packages/client/components/instance/Instance.ts
@@ -1,9 +1,11 @@
 export default class Instance {
   readonly apiUrl: string;
   readonly wsUrl: string;
+  readonly mediaUrl: string;
 
-  constructor(apiUrl: string, wsUrl: string) {
+  constructor(apiUrl: string, wsUrl: string, mediaUrl: string) {
     this.apiUrl = apiUrl;
     this.wsUrl = wsUrl;
+    this.mediaUrl = mediaUrl;
   }
 }
