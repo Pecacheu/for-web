2c501e6b54d080106e1e196fa6005ab80c4b4d9d
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-03-14 02:42:04 -0400

    fix: Misc console warnings - Fix 'scrollTo undefined' error in Message - Fix 'computations will never be disposed' warning in Container - Add excluded generated dirs from eslint config to vscode config
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- packages/client/components/ui/components/features/messaging/elements/Container.tsx
+++ packages/client/components/ui/components/features/messaging/elements/Container.tsx
@@ -363,7 +363,7 @@ export function MessageContainer(props: Props) {
                   use:floating={{
                     tooltip: {
                       placement: "top",
-                      content: (
+                      content: () => (
                         <>
                           {t`Sent`}{" "}
                           <Time
@@ -372,7 +372,8 @@ export function MessageContainer(props: Props) {
                             referenceTime={props._referenceTime}
                           />
                         </>
-                      ) as string, // ignore aria requirement
+                      ),
+                      aria: "",
                     },
                   }}
                 >
@@ -392,7 +393,7 @@ export function MessageContainer(props: Props) {
                 use:floating={{
                   tooltip: {
                     placement: "top",
-                    content: (
+                    content: () => (
                       <Column>
                         <span>
                           {t`Sent`}{" "}
@@ -413,7 +414,8 @@ export function MessageContainer(props: Props) {
                           </span>
                         </Show>
                       </Column>
-                    ) as string, // ignore aria requirement
+                    ),
+                    aria: "",
                   },
                 }}
               >
@@ -436,39 +438,41 @@ export function MessageContainer(props: Props) {
               <NonBreakingText>
                 <div class={infoText()}>
                   {props.info}
-                  <Switch fallback={props.timestamp as string}>
-                    <Match when={props.timestamp instanceof Date}>
-                      <span
-                        use:floating={{
-                          tooltip: {
-                            placement: "top",
-                            content: (
-                              <>
-                                {t`Sent`}{" "}
-                                <Time
-                                  format="datetime"
-                                  value={props.timestamp}
-                                  referenceTime={props._referenceTime}
-                                />
-                              </>
-                            ) as string, // ignore aria requirement
-                          },
-                        }}
-                      >
-                        <Time
-                          format="calendar"
-                          value={props.timestamp}
-                          referenceTime={props._referenceTime}
-                        />
-                      </span>
-                    </Match>
-                  </Switch>
+                  <Show
+                    when={props.timestamp instanceof Date}
+                    fallback={props.timestamp as JSX.Element}
+                  >
+                    <span
+                      use:floating={{
+                        tooltip: {
+                          placement: "top",
+                          content: () => (
+                            <>
+                              {t`Sent`}{" "}
+                              <Time
+                                format="datetime"
+                                value={props.timestamp}
+                                referenceTime={props._referenceTime}
+                              />
+                            </>
+                          ),
+                          aria: "",
+                        },
+                      }}
+                    >
+                      <Time
+                        format="calendar"
+                        value={props.timestamp}
+                        referenceTime={props._referenceTime}
+                      />
+                    </span>
+                  </Show>
                   <Show when={props.edited}>
                     <span
                       use:floating={{
                         tooltip: {
                           placement: "top",
-                          content: (
+                          content: () => (
                             <>
                               {t`Edited`}{" "}
                               <Time
@@ -477,7 +481,8 @@ export function MessageContainer(props: Props) {
                                 referenceTime={props._referenceTime}
                               />
                             </>
-                          ) as string, // ignore aria requirement
+                          ),
+                          aria: "",
                         },
                       }}
                     >
